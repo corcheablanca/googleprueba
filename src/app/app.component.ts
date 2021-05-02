e212a1da86bbe7d8c47861d716633010003ffbfa
corcheablanca <spintogarcia@gmail.com>
2021-05-02 17:09:29 -0500

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  latitude = 37.09024;
+  longitude = -95.712891;
+}
